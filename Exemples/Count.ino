fbbf82ad6618192cb7c71876ebf0fcf88c6c0c8c
Amaf14 <alexmaf14@yahoo.com>
2016-03-23 09:41:55 +0200

    Update Count.ino

--- Exemples/Count.ino
+++ Exemples/Count.ino
@@ -1,6 +1,16 @@
+/*********************************************************************
+This is an example sketch for a BCD driver.
+
+The driver uses four pins to comunnicate with the Arduino.
+
+Written by Amaf14.
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
+
 #include<CD4511.h>
 
-CD4511 7segment=(9, 8, 7, 6, 5);
+CD4511 7segment=(9, 8, 7, 6, 5);//A=9, B=8, C=7, D=6 
 //uses a single digit display
 //the function for a single digit isn't done yet
 int k;
