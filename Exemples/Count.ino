6b2b3ec59e724aa4bbdbd8c92e787a55f5a528b3
Amaf14 <alexmaf14@yahoo.com>
2016-03-23 09:32:14 +0200

    Update Count.ino

--- Exemples/Count.ino
+++ Exemples/Count.ino
@@ -1,19 +1,21 @@
 #include<CD4511.h>
 
 CD4511 7segment=(9, 8, 7, 6, 5);
+//uses a single digit display
+//the function for a single digit isn't done yet
 int k;
 
 void setup()
 {
-   7sgment.start();
-   7segmen.binlist();
+   7sgment.start();//making the selected pins outputs
+   7segmen.binlist();//a list with numbers from 0 to 9 in binary code
 }
 
 void loop()
 {
    for(k=0 ; k<=9 ; k++)
    {
-      7segment.display(v[k], D1);
-      delay(1000);
+      7segment.display(v[k], D1);//display the number 
+      delay(1000);//wait one second
    }
 }
